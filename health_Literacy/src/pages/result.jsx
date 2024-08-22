86938ef484d0b4adc25e1e7a5034bf38870074ff
Tnkwp <119997936+Tnkwp@users.noreply.github.com>
2024-08-22 14:03:18 +0700

    submit

--- health_Literacy/src/pages/result.jsx
+++ health_Literacy/src/pages/result.jsx
@@ -8,35 +8,35 @@ function ResultScreen() {
 
       
       <div className="flex flex-col p-10 mt-20 text-center ">
-        <div className="bg-green-500 p-4 rounded-lg mb-4 text-white">
-            <div className="flex justify-center mb-4 w-[4rem] mx-auto">
+        <div className="bg-green-500 p-4 rounded-lg mb-4 text-white h-[20rem] ">
+            <div className="flex justify-center mb-4 mt-12 w-[4rem]  mx-auto">
                     <img src="/public/ผลประเมิน.png" alt="/" />
             </div>
-            <div className="text-2xl font-bold">
+            <div className="text-3xl font-bold">
                 <h2>ผลการประเมิน Health Literacy</h2>
                 <p>การแสดงผลแบบแผนที่</p>
             </div>
-                <p>คลิกเพื่อดูผลการประเมิน</p>
+                <p className='text-xl'>คลิกเพื่อดูผลการประเมิน</p>
         </div>
-        <Link to={"/healthAssessmentTable"} ><div className="bg-yellow-500 p-4 rounded-lg mb-4 text-white">
-            <div className="flex justify-center mb-4 w-[4rem] mx-auto">
+        <Link to={"/healthAssessmentTable"} ><div className="bg-yellow-500 p-4 rounded-lg mb-4 text-white h-[20rem]">
+            <div className="flex justify-center mb-4 mt-12 w-[4rem] mx-auto">
                     <img src="/public/ผลประเมิน.png" alt="/" />
             </div>
-            <div className="text-2xl font-bold ">
+            <div className="text-3xl font-bold ">
                 <h2>ผลการประเมิน Health Literacy</h2>
                 <p>การแสดงผลแบบตาราง</p>
             </div>
-          <p>คลิกเพื่อดูผลการประเมิน</p>
+          <p className='text-xl'>คลิกเพื่อดูผลการประเมิน</p>
         </div></Link>
-        <Link to={"/chart"}><div className="bg-red-500 p-4 rounded-lg text-white">
-            <div className="flex justify-center mb-4 w-[4rem] mx-auto">
-                    <img src="/public/ผลประเมิน.png" alt="" />
+        <Link to={"/chart"}><div className="bg-red-500 p-4 rounded-lg text-white h-[20rem]">
+            <div className="flex justify-center mb-4 mt-12 w-[4rem] mx-auto">
+                    <img src="/public/ผลประเมิน.png" alt="/" />
             </div>
-            <div className="text-2xl font-bold ">
+            <div className="text-3xl font-bold ">
                 <h2>ผลการประเมิน Health Literacy</h2>
                 <p>การแสดงผลแบบแผนภูมิแท่ง</p>
             </div>
-          <p>คลิกเพื่อดูผลการประเมิน</p>
+          <p className='text-xl'>คลิกเพื่อดูผลการประเมิน</p>
         </div></Link>
       </div>
     </div>
